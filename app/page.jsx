6d60e5879363bfd135fddadd69ad1a510fb14943
oncprompt <onc.genius@gmail.com>
2024-05-26 13:22:40 +0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <section>
+    <div className="container">
+      <h1 className="text-3xl font-bold">Next Js 14 Starter KIT</h1>
+    </div>
+   </section>
+  );
+}
